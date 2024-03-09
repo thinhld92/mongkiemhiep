dedf36b52063e994c60b0edc5e190362105f2fcf
Thinh Le <thinhld92@gmail.com>
2024-03-09 09:28:28 +0700

    update giao diện nạp

--- resources/views/hotro/payments/create.blade.php
+++ resources/views/hotro/payments/create.blade.php
@@ -17,7 +17,7 @@
         </div>
         <div class="col-12 mb-4">
           <ul class="text-danger">
-            <li><span class="text-danger">1000 VND 🚀🚀 10 xu</span></li>
+            <li><span class="text-danger">100k VND 🚀🚀 1k xu</span></li>
             {{-- <li><span class="text-danger"><strong>🎉Nạp lần đầu được x2 số xu🎉</strong></span></li> --}}
             <li><span class="text-danger">Trên 500k thêm 20%, 1 triệu được thêm 30%, trên 2 triệu được thêm 50%</span></li>
           </ul>
